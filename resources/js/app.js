d879c361ab23138463db1e696f3c135cf42d8d70
technosystem <technosystem2123@gmail.com>
2022-02-12 18:12:33 -0400

    Integracion LARAVEL 9

---
+++ resources/js/app.js
@@ -0,0 +1,69 @@
+require('./bootstrap');
+
+/* pace-progress */
+window.Pace = require('@dlghq/pace-progress');
+
+
+
+ window.feather = require('feather-icons')
+ feather.replace();
+
+ require('vuexy/app-assets/vendors/js/charts/chart.min.js');
+
+/* toastr */
+window.toastr = require('vuexy/app-assets/vendors/js/extensions/toastr.min.js')
+toastr.options = {
+
+  "closeButton": false,
+  "debug": false,
+  "newestOnTop": false,
+  "progressBar": true,
+  "positionClass": "toast-top-right",
+  "preventDuplicates": true,
+  "onclick": null,
+  "showDuration": 300,
+  "hideDuration": 1000,
+  "timeOut": 5000,
+  "extendedTimeOut": 1000,
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "slideDown", 
+  "hideMethod": "fadeOut"
+}
+
+//Icheck
+require('icheck');
+
+
+    
+require('vuexy/app-assets/vendors/js/tables/datatable/jquery.dataTables.min.js');
+require('vuexy/app-assets/vendors/js/tables/datatable/datatables.bootstrap4.min.js');
+require('vuexy/app-assets/vendors/js/tables/datatable/dataTables.responsive.min.js');
+require('vuexy/app-assets/vendors/js/tables/datatable/responsive.bootstrap4.js');
+require('vuexy/app-assets/vendors/js/tables/datatable/datatables.checkboxes.min.js');
+require('vuexy/app-assets/vendors/js/tables/datatable/datatables.buttons.min.js');
+require('vuexy/app-assets/vendors/js/tables/datatable/jszip.min.js');
+require('vuexy/app-assets/vendors/js/tables/datatable/pdfmake.min.js');
+require('vuexy/app-assets/vendors/js/tables/datatable/vfs_fonts.js');
+require('vuexy/app-assets/vendors/js/tables/datatable/buttons.html5.min.js');
+require('vuexy/app-assets/vendors/js/tables/datatable/buttons.print.min.js');
+require('vuexy/app-assets/vendors/js/tables/datatable/dataTables.rowGroup.min.js');
+require('vuexy/app-assets/vendors/js/forms/validation/jquery.validate.min.js');
+require('vuexy/app-assets/vendors/js/charts/apexcharts.min.js');
+require('vuexy/app-assets/vendors/js/extensions/toastr.min.js');
+require('vuexy/app-assets/js/scripts/pages/page-profile.js');
+require('vuexy/app-assets/vendors/js/forms/select/select2.full.min.js');  
+require('vuexy/app-assets/js/scripts/forms/form-select2.js'); 
+require('vuexy/app-assets/vendors/js/pickers/pickadate/picker.js');
+require('vuexy/app-assets/vendors/js/pickers/pickadate/picker.date.js');
+require('vuexy/app-assets/vendors/js/pickers/pickadate/picker.time.js');
+require('vuexy/app-assets/vendors/js/pickers/pickadate/legacy.js');
+require('vuexy/app-assets/vendors/js/pickers/flatpickr/flatpickr.min.js');
+require('vuexy/app-assets/js/scripts/forms/pickers/form-pickers.js');
+
+
+
+
+
+
+
